ba42c4758935f64186a729595fa8fe84fbc9c944
leijun <lei.jun@manmanh.com>
2018-06-26 09:37:38 +0800

    更新配置文件

--- config.py
+++ config.py
@@ -13,16 +13,31 @@ class Config():
 
 
 class DevConfig(Config):
-    UPLOAD_FOLD = 'F://upload/'#上传文件目录
-    DOWNLOAD_FOLD = "F://download"#下载模板目录
+    LOG_FILENAME = '/Users/newcity/PycharmProjects/webill-charge/celery.log'
+    LOG_FILESIZE = 1024*1024*10 #10M
+
+    UPLOAD_FOLD = '/Users/newcity/Uploads/'#上传文件目录
+    DOWNLOAD_FOLD = "/Users/newcity/Uploads/"#下载模板目录
     TOKEN_TEST_URL = 'http://yadong.test.manmanh.com/webill-app/api/user/checkToken'#TOKEN验证api地址
-    TOKEN_TEST_BUTTON = 0#TOKEN验证开关
-    SQLHELPER={'host':'127.0.0.1','username':'root','passwd':'zx1994617','db':'testsql','port':3306}
+    TOKEN_TEST_BUTTON = 0 # TOKEN验证开关
     DEBUG=False
-    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:zx1994617@127.0.0.1:3306/testsql?charset=utf8'
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://dev:devdb@192.168.98.133:3306/webill-test?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     MONGODB_SETTINGS = {'db':'Datatable','alias':'default'}
     # MONGODB_SETTINGS = {'db':'Datatable','host': '192.168.98.133', 'port': 27017}
+
+    CELERY_BROKER_URL = 'redis://mx_redis:6379/0'
+
+    #celery 设置
+    broker_url = 'redis://127.0.0.1:6379/0'
+    result_backend = 'redis://127.0.0.1:6379/0'
+    task_serializer = 'json'
+    result_serializer = 'json'
+    accept_content = ['json']
+    #timezone = 'Europe/Oslo'
+    enable_utc = True
+
+
     JOBS = [
         {
             'id': 'daily_quest',
@@ -31,8 +46,8 @@ class DevConfig(Config):
             'trigger': {
                 'type': 'cron',
                 'day_of_week': "mon-sun",
-                'hour': '14',
-                'minute': '09',
+                'hour': '19',
+                'minute': '27',
                 'second': '0'
             },
         }
